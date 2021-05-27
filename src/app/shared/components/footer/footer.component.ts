1fa472e103537d14bab513af89bba8b3e6c3333d
Hector Romero <hectorromerodev@gmail.com>
2021-05-27 13:49:23 -0700

    UPD footer component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  template: `
+  <footer>
+    <p>
+      {{message}}
+      <a href="{{url}}" rel="noopener" target="_blank" >{{name}}</a>
+    </p>
+  </footer>
+  `
 })
 export class FooterComponent implements OnInit {
+  @Input() url: string = 'https://merakideveloper.com/';
+  @Input() message: string = '© 2021, created by'
+  @Input() name: string = 'Meraki Developer'
 
   constructor() { }
 
