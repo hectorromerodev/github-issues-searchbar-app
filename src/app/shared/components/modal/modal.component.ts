6ab9a22bcb07bd4289af8954ec1b74f4451c61a9
Hector Romero <hectorromerodev@gmail.com>
2021-05-27 19:01:29 -0700

    UPD modal component

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ModalService } from '@core/index';
 @Component({
-  selector: 'app-modal',
+  selector: 'popup-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
+  @Input() id!: string;
+  private element: HTMLElement;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private modalServ: ModalService,
+    private readonly ele: ElementRef,
+    @Inject(DOCUMENT) private doc: Document
+  ) {
+    this.element = this.ele.nativeElement;
   }
 
+  ngOnInit(): void {
+    // Ensure id attribute exists
+    if (!this.id) {
+      console.error('Modal must have an id');
+      return;
+    }
+    // Move element to bottom of page (just before </body>) so it cana be  displayer above everything else
+    this.doc.body.appendChild(this.element);
+    // close modal on background click
+    this.element.addEventListener('click', (el: any) => {
+      if (el.target.className === 'popup-mdoal') {
+        this.close();
+      }
+    });
+
+    // add self (this modal instance) to the modal service so it's a accessible from controllers
+    this.modalServ.add(this);
+  }
+
+  // Remove self from service
+  ngOnDestroy(): void {
+    this.modalServ.remove(this.id);
+    this.element.remove();
+  }
+
+  // Open modal
+  open(): void {
+    this.element.style.display = 'block';
+    this.doc.body.classList.add('popup-modal-open');
+  }
+
+  // Close modal
+  close(): void {
+    this.element.style.display = 'none';
+    this.doc.body.classList.remove('popup-modal-open');
+  }
 }
