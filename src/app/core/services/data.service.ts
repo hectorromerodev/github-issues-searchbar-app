944f3e0b3d18d0a8f3b1c835890b8620ae905177
Hector Romero <hectorromerodev@gmail.com>
2021-05-28 16:41:31 -0700

    UPD data service

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,42 +1,39 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { catchError, pluck, take, tap } from 'rxjs/operators';
+import { BehaviorSubject, of } from 'rxjs';
+
 import { Injectable } from '@angular/core';
 import { environment } from '@environment/environment';
 import { Issue } from '@shared/interfaces/issue.interface';
-import { BehaviorSubject, of } from 'rxjs';
-import { catchError, pluck, take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private endpoint = `${environment.BASE_URL}/${environment.DEFAULT_USER_REPO}/${environment.ISSUES}`;
+  private endpoint = `${environment.BASE_URL}/${environment.SEARCH}/${environment.ISSUES}`;
+  private readonly HEADERS = new HttpHeaders().set('accept', 'application/vnd.github.v3+json')
 
   private issuesBehaviorSub: BehaviorSubject<Issue[]> = new BehaviorSubject<Issue[]>([])
   issues$ = this.issuesBehaviorSub.asObservable();
 
-  constructor(
-    private http: HttpClient
-  ) {
-    this.getIssues();
-  }
+  constructor(private http: HttpClient) { }
 
-  getIssues(): void {
+  filterIssueByTitle(title: string | number): void {
     const params = new HttpParams()
-      .set('per_page', 20);
-    const headers = new HttpHeaders()
-      .set('accept', 'application/vnd.github.v3+json')
-
-    this.http.get<any>(this.endpoint, { params, headers })
+      .set('per_page', 30)
+      .set('q', title);
+    this.http.get<any>(this.endpoint, { params, headers: this.HEADERS })
       .pipe(
         take(1),
-        tap((issues: Issue[]) => {
-          this.issuesBehaviorSub.next(issues)
-          // console.log(issues)
-        }),
+        pluck('items'),
+        tap((issue: Issue[]) => this.issuesBehaviorSub.next(issue)),
         catchError(error => {
-          console.warn('GetIssues Error: ', error);
-          return of([])
+          console.warn('GetIssuesByTitle Error: ', error);
+          return of([]);
         })
       ).subscribe();
+  }
+  reset(): void {
+    this.issuesBehaviorSub.next([]);
   }
 }
