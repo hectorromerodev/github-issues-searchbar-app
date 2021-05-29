9419abf8514c7b73f1c3e01c7fc813285040ec8a
Hector Romero <hectorromerodev@gmail.com>
2021-05-28 17:19:55 -0700

    UPD searchbar component

--- src/app/shared/components/searchbar/searchbar.component.ts
+++ src/app/shared/components/searchbar/searchbar.component.ts
@@ -5,7 +5,15 @@ import { Subject } from 'rxjs';
 import { tap, map, debounceTime, distinctUntilChanged, filter, takeUntil } from 'rxjs/operators'
 @Component({
   selector: 'app-searchbar',
-  templateUrl: './searchbar.component.html',
+  template: `
+    <section class="searchbar__container">
+      <div class="searchbar__input">
+        <label for="search">Search an issue by name...</label>
+        <input [formControl]="search" type="text" role="searchbox" placeholder="Search an issue by name...">
+        <button (click)="onClear()" class="button">X</button>
+      </div>
+    </section>
+  `,
   styleUrls: ['./searchbar.component.scss']
 })
 export class SearchbarComponent implements OnDestroy {
@@ -27,15 +35,16 @@ export class SearchbarComponent implements OnDestroy {
     this.search.valueChanges
       .pipe(
         map(search => search?.toLowerCase().trim()),
-        debounceTime(400),
+        debounceTime(200),
         distinctUntilChanged(),
         filter(search => search !== '' && search?.length > 2),
-        tap(search => console.log(search)),//this.dataServ.filterCharacters(search)),
+        tap(search => this.dataServ.filterIssueByTitle(search)),
         takeUntil(this.destroy$)
       ).subscribe();
   }
 
   onClear(): void {
     this.search.reset();
+    this.dataServ.reset();
   }
 }
